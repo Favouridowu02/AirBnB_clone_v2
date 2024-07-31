445727fb44dd3bc9a31b4ab8535011a731dd8856
Favour Idowu <favouridowu02@gmail.com>
2024-07-31 21:45:58 +0100

    Wrote an sql script for the the test db

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,7 +1,8 @@
 -- A Script that prepares a MySQL server for the project
 -- This is the test mode
 
-CREATE DATABASE hbnb_test_db IF NOT EXISTS;
-CREATE USER 'hbnb_test'@'localhost' WITH PRIVILEGES 'hbnb_test_pwd' IF NOT EXISTS;
-GRANT ALL PRIVILEGES ON 'hbnb_test_db' TO 'hbnb_test'@'localhost';
-GRANT SELECT PRIVILEGES ON 'performance_schema' TO 'hbnb_test'@'localhost';
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+USE hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
