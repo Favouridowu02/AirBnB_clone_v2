593f0a6f3a4ea9d5d3457f19075ea5e1288e4dc0
Favour Idowu <favouridowu02@gmail.com>
2024-08-20 14:03:11 +0100

    Update the class models Place, User, City
    Updated the class model Place, User and City to implement better features and created a one to many relationship amongst them. Also implemented a feature that once the Model with the one relationship (Parent) i.e User and City is deleted the child i.e Place will be deleted

--- models/city.py
+++ models/city.py
@@ -2,7 +2,9 @@
 """ City Module for HBNB project """
 from models.base_model import BaseModel
 from models.base_model import Base
+from models.place import Place
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
@@ -11,3 +13,4 @@ class City(BaseModel, Base):
 
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship('Place', backref='cities', cascade="all delete-orphan")
