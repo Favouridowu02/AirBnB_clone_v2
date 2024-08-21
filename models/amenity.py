b9deccfb1f1d3dc04a3e539a66a355600d29ca08
Favour Idowu <favouridowu02@gmail.com>
2024-08-21 21:14:53 +0100

    Update the class models Place, Amenity
    Updated Place to created a many to many relationship with Amenity. Created a Table called place_amenity used for the secondary. Added some methods to the Place class model for getting and setting the amenity_ids list

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from models.place import Place, place_amenity
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """
+        This is the Amenities Class
+
+        Attributes:
+            name: The is the name of the amenities
+            place_amenities: This is the place amenities attribute.
+    """
+    __tablename__="amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship('Place', secondary=place_amenity, viewonly=False)
--- models/place.py
+++ models/place.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, ForeignKey, Float
+from sqlalchemy import Column, String, Integer, ForeignKey, Float, Table
 from sqlalchemy.orm import relationship
 
 from os import getenv
 from models.review import Review
+from models.amenity import Amenity
+
+place_amenity = Table('place_amenity', Base.metadata,
+    Column('place_id', String(60), ForeignKey('places.id'), nullable=False, primary_key=True),
+    Column('amenity_id', String(60), ForeignKey('amenities.id'), nullable=False, primary_key=True),
+)
 
 
 class Place(BaseModel, Base):
@@ -25,7 +31,9 @@ class Place(BaseModel, Base):
 
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         reviews= relationship('Review', backref='place', cascade='all, delete-orphan')
+        amenities = relationship('Amenity', secondary=place_amenity, viewonly=False)
     else:
+        
         @property
         def reviews(self):
             """
@@ -45,5 +53,27 @@ class Place(BaseModel, Base):
                 if obj.place_id == self.id:
                     new_list.append(obj)
             return new_list
-
         
+        @property
+        def amenities(self):
+            """
+                A getter Attribute that returns the list of Amenity instances based on the
+                attribute amenity_ids that contains all Amenity.id linked to the Place
+            """
+            from models import storage
+            amenities_list = storage.all(Amenity)
+            new_list = []
+            for obj in amenities_list.values():
+                if obj.id in self.amenity_ids:
+                    new_list.append(obj)
+            return new_list       
+
+        @amenities.setter
+        def amenities(self, obj):
+            """
+                Setter attribute amenities that handles append method for adding an Amenity.id
+                to the attribute amenity_ids. This method should accept only Amenity object,
+                otherwise, do nothing.
+            """
+            if type(obj) == Amenity:
+                self.amenity_ids.append(obj.id)
