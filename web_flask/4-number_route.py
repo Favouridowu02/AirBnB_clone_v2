80d674dbc6189a24794fb56c4ef8ea09a8617d8a
Favour Idowu <favouridowu02@gmail.com>
2024-08-30 20:41:39 +0100

    A script that Collects dynamic input from the user url when route='/python/<text>'. text is equal to is_cool by default. When route='/number/<int:n>  n must be an integer

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+    This Module contains a script that starts a Flask web application
+    Routes:
+        home
+        route
+        c: a dynamic route that gets the text for the url
+        python: a dynamic route that gets the text for the url, If
+        none is passed text ="is magic"
+        n: a dynamix route that only collect numbers
+
+    Option: strict_slashes=False
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    text = text.replace('_', ' ')
+    return f"C {text}"
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text="is_cool"):
+    text = text.replace('_', ' ')
+    return f"Python {text}"
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    return f"{n} is a number"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
