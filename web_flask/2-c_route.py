9d446796cbd455ce67ebc4bd7ab4aabf7c8513b8
Favour Idowu <favouridowu02@gmail.com>
2024-08-30 20:23:06 +0100

    A script that Collects dynamic input from the user url

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+    This Module contains a script that starts a Flask web application
+    Routes:
+        home
+        route
+        c: a dynamix route that gets the text for the url
+
+    Option: strict_slashes=False
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    text = text.replace('_', ' ')
+    return f"c {text}"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
