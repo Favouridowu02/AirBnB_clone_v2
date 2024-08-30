21e7c22defe84044b164235483aff93c326500ea
Favour Idowu <favouridowu02@gmail.com>
2024-08-30 20:25:43 +0100

    A script that Collects dynamic input from the user url

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -26,7 +26,7 @@ def hbnb():
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
     text = text.replace('_', ' ')
-    return f"c {text}"
+    return f"C {text}"
 
 
 if __name__ == "__main__":
