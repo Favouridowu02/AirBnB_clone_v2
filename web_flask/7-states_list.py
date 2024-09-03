ef1344af6f48e91d7dcfa563e1cf6da168e77ca4
Favour Idowu <favouridowu02@gmail.com>
2024-09-03 05:39:26 +0100

    Add a script that fetches from the database and renders to the user CLient
    Added a script that fetches data from the database and renders the data to the user side. This data is all the states stored in the database and are sorted out in ascending order based on the the states name

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+    This module Contains a python flask scrip to fetch from the mysql database
+
+    Routes:
+        home: returns 'I am Home'
+
+"""
+import sys
+import os
+
+# Add the parent directory to sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+print(State)
+
+states = storage.all(State).values()
+print(states)
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def home():
+    """The Home Page of this web app"""
+    return "I am Home"
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """display a HTML page with the states listed in alphabetical order"""
+    states = list(storage.all(State).values())
+    states = sorted(states, key=lambda state: state.name)
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def close(exception=True):
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port='5000')
